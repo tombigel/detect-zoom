cbe2f768eb6b03064e4f131cefb652d9af8fb09b
Tom Bigelajzen <public@tombigel.com>
2013-01-26 17:17:02 +0200

    forgot to return the values in firefox18() fixed

--- detect-zoom.js
+++ detect-zoom.js
@@ -21,7 +21,7 @@
      * Use devicePixelRatio if supported by the browser
      * @return {Number}
      */
-    var devicePixelRatio = function(){
+    var devicePixelRatio = function () {
         return window.devicePixelRatio || 1;
     };
 
@@ -36,10 +36,10 @@
      * @param epsilon
      * @return {Number}
      */
-    var mediaQueryBinarySearch = function(property, unit, a, b, maxIter, epsilon){
+    var mediaQueryBinarySearch = function (property, unit, a, b, maxIter, epsilon) {
         var matchMedia;
         var head, style, div;
-        if (window.matchMedia){
+        if (window.matchMedia) {
             matchMedia = window.matchMedia;
         } else {
             head = document.getElementsByTagName('head')[0];
@@ -51,7 +51,7 @@
             div.style.display = 'none';
             document.body.appendChild(div);
 
-            matchMedia = function(query){
+            matchMedia = function (query) {
                 style.sheet.insertRule('@media ' + query + '{.mediaQueryBinarySearch ' + '{text-decoration: underline} }', 0);
                 var matched = getComputedStyle(div, null).textDecoration == 'underline';
                 style.sheet.deleteRule(0);
@@ -59,19 +59,19 @@
             };
         }
         var ratio = binarySearch(a, b, maxIter);
-        if (div){
+        if (div) {
             head.removeChild(style);
             document.body.removeChild(div);
         }
         return ratio;
 
-        function binarySearch(a, b, maxIter){
+        function binarySearch(a, b, maxIter) {
             var mid = (a + b) / 2;
-            if (maxIter <= 0 || b - a < epsilon){
+            if (maxIter <= 0 || b - a < epsilon) {
                 return mid;
             }
             var query = "(" + property + ":" + mid + unit + ")";
-            if (matchMedia(query).matches){
+            if (matchMedia(query).matches) {
                 return binarySearch(mid, b, maxIter - 1);
             } else {
                 return binarySearch(a, mid, maxIter - 1);
@@ -85,10 +85,10 @@
      * @return {Object}
      * @private
      **/
-    var ie8 = function(){
-        var zoom = Math.round ((screen.deviceXDPI / screen.logicalXDPI) * 100);
+    var ie8 = function () {
+        var zoom = Math.round((screen.deviceXDPI / screen.logicalXDPI) * 100);
         return {
-            zoom            : zoom,
+            zoom: zoom,
             devicePxPerCssPx: zoom * devicePixelRatio()
         };
     };
@@ -101,11 +101,11 @@
      * @return {Object}
      * @private
      */
-    var webkitMobile = function(){
+    var webkitMobile = function () {
         var deviceWidth = (Math.abs(window.orientation) == 90) ? screen.height : screen.width;
         var zoom = deviceWidth / window.innerWidth;
         return {
-            zoom            : zoom,
+            zoom: zoom,
             devicePxPerCssPx: zoom * devicePixelRatio()
         };
     };
@@ -126,15 +126,15 @@
      * @return {Object}
      * @private
      */
-    var webkit = function(){
+    var webkit = function () {
 
-        var important = function(str){
+        var important = function (str) {
             return str.replace(/;/g, " !important;");
         };
 
         var div = document.createElement('div');
-            div.innerHTML = "1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>0";
-            div.setAttribute('style', important('font: 100px/1em sans-serif; -webkit-text-size-adjust: none; height: auto; width: 1em; padding: 0; overflow: visible;'));
+        div.innerHTML = "1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>0";
+        div.setAttribute('style', important('font: 100px/1em sans-serif; -webkit-text-size-adjust: none; height: auto; width: 1em; padding: 0; overflow: visible;'));
 
         // The container exists so that the div will be laid out in its own flow
         // while not impacting the layout, viewport size, or display of the
@@ -142,16 +142,16 @@
         // Add !important and relevant CSS rule resets
         // so that other rules cannot affect the results.
         var container = document.createElement('div');
-            container.setAttribute('style', important('width:0; height:0; overflow:hidden; visibility:hidden; position: absolute;'));
-            container.appendChild(div);
+        container.setAttribute('style', important('width:0; height:0; overflow:hidden; visibility:hidden; position: absolute;'));
+        container.appendChild(div);
 
         document.body.appendChild(container);
         var zoom = 1000 / div.clientHeight;
-            zoom = Math.round(zoom * 100) / 100;
+        zoom = Math.round(zoom * 100) / 100;
         document.body.removeChild(container);
 
         return{
-            zoom            : zoom,
+            zoom: zoom,
             devicePxPerCssPx: zoom * devicePixelRatio()
         };
     };
@@ -164,11 +164,11 @@
      * @return {Object}
      * @private
      */
-    var firefox4 = function(){
+    var firefox4 = function () {
         var zoom = mediaQueryBinarySearch('min--moz-device-pixel-ratio', '', 0, 10, 20, 0.0001);
-            zoom = Math.round(zoom * 100) / 100;
+        zoom = Math.round(zoom * 100) / 100;
         return {
-            zoom            : zoom,
+            zoom: zoom,
             devicePxPerCssPx: zoom * devicePixelRatio()
         };
     };
@@ -177,11 +177,14 @@
      * Firefox 18.x
      * Mozilla added support for devicePixelRatio to Firefox 18,
      * but it is affected by the zoom level.
-     * To normalize it we need to divide the pixel ratio by zoom level.
+     * To normalize it we need to divide the pixel ratio by zoom level (And we can't bypass the binary search yet...).
      */
-    var firefox18 = function(){
+    var firefox18 = function () {
         var zoomObj = firefox4();
-        zoomObj.devicePxPerCssPx = devicePixelRatio() / zoomObj.zoom;
+        return {
+            zoom: zoomObj.zoom,
+            devicePxPerCssPx: devicePixelRatio() / zoomObj.zoom
+        };
     };
 
     /**
@@ -192,44 +195,44 @@
      * @return {Object}
      * @private
      */
-    var opera11 = function(){
+    var opera11 = function () {
         var zoom = window.outerWidth / window.innerWidth;
-            zoom = Math.round(zoom * 100) / 100;
+        zoom = Math.round(zoom * 100) / 100;
         return {
-            zoom            : zoom,
+            zoom: zoom,
             devicePxPerCssPx: zoom * devicePixelRatio()
         };
     };
 
-    var ratios = function(){
+    var ratios = function () {
         var ratio = {
-            zoom            : 1,
+            zoom: 1,
             devicePxPerCssPx: 1
         };
 
         //IE8+
-        if (!isNaN(screen.logicalXDPI) && !isNaN(screen.systemXDPI)){
+        if (!isNaN(screen.logicalXDPI) && !isNaN(screen.systemXDPI)) {
             ratio = ie8();
 
-        //Mobile Webkit
-        } else if ('ontouchstart' in window && typeof document.body.style.webkitTextSizeAdjust === 'string'){
+            //Mobile Webkit
+        } else if ('ontouchstart' in window && typeof document.body.style.webkitTextSizeAdjust === 'string') {
             ratio = webkitMobile();
 
-        //WebKit
-        } else if (typeof document.body.style.webkitTextSizeAdjust === 'string'){
+            //WebKit
+        } else if (typeof document.body.style.webkitTextSizeAdjust === 'string') {
             ratio = webkit();
 
-        //Opera
-        } else if (navigator.userAgent.indexOf('Opera') >= 0){
+            //Opera
+        } else if (navigator.userAgent.indexOf('Opera') >= 0) {
             ratio = opera11();
 
-        //Last one is Firefox
-        //FF 18.x
-        } else if (window.devicePixelRatio){
+            //Last one is Firefox
+            //FF 18.x
+        } else if (window.devicePixelRatio) {
             ratio = firefox18();
 
-        //FF 4.0 - 17.x
-        } else if (firefox4().zoom > 0.001){
+            //FF 4.0 - 17.x
+        } else if (firefox4().zoom > 0.001) {
             ratio = firefox4();
 
         }
@@ -243,7 +246,7 @@
          * Ratios.zoom shorthand
          * @return {Number} Zoom level
          */
-        zoom: function(){
+        zoom: function () {
             return ratios().zoom;
         },
 
@@ -251,7 +254,7 @@
          * Ratios.devicePxPerCssPx shorthand
          * @return {Number} devicePxPerCssPx level
          */
-        device: function(){
+        device: function () {
             return ratios().devicePxPerCssPx;
         }
     });
